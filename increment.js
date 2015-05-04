f089fbdc0a744a81f80742a853db4a584b1408d4
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-05-04 15:00:14 -0400

    Answer for increment question

---
+++ increment.js
@@ -0,0 +1,24 @@
+//EricHeinzman 
+//Answer for Question 1 
+//Create a function that increments numbers before multiplying them
+
+
+
+function inc(x){
+
+ x = 0
+ while(x= 0;)
+  {
+	x +10}
+
+function multiply(a,b){
+
+	a = 10;
+	b = 20;
+
+  inc(x){
+	a +1;
+	b+1;
+
+}
+ 
